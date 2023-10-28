fa46131d037ea6917cd646894f7d725a9915e925
tedy <tedy69@gmail.com>
2023-10-28 07:05:53 +0200

    add chart

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,13 +13,14 @@ import { formatCurrency } from './utils';
 export async function fetchRevenue() {
   // Add noStore() here prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
+  // noStore()
 
   try {
     // Artificially delay a reponse for demo purposes.
     // Don't do this in real life :)
 
     // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
